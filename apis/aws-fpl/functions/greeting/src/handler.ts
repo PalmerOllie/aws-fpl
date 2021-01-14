e18162854682cbc3da6c55146b5517f711af5363
PalmerOllie <palmerollie@gmail.com>
2021-01-14 11:49:36 +0000

    Added API endpoint for Prod/hello

--- apis/aws-fpl/functions/greeting/src/handler.ts
+++ apis/aws-fpl/functions/greeting/src/handler.ts
@@ -3,5 +3,15 @@ import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
 import response from '/opt/nodejs/defaultResponses';
 
 export default async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-  return Promise.resolve(response.success(200, {}, { message: 'Hello World!' }));
-};
+    return Promise.resolve(
+        {
+            statusCode: 200,
+            headers: {
+                "some-custom-header":"some custom value"
+            },
+            body: JSON.stringify('Hello World!') 
+        });
+    //     response.success(
+    //         200, {}, { message: 'Hello World!' })
+    // );
+}
